147a4cf653c9569be2c01872779aeef3aa9777fc
Brij Yadav <Brij.Yadav@vitalitygroup.com>
2022-09-13 07:32:37 +0200

    first commit

---
+++ src/main/java/net/javaguides/springboot/SpringbootCrudRestfulWebservicesAppApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudRestfulWebservicesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudRestfulWebservicesAppApplication.class, args);
+	}
+
+}
